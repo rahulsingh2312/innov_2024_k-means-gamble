21091df32603a0cfce37867572506709a973404f
rahulsingh2312 <rahulsinghhh2312@gmail.com>
2024-02-27 17:18:17 +0530

    chat bot'

--- markinsights-frontend/src/app/Bot/Bot.js
+++ markinsights-frontend/src/app/Bot/Bot.js
@@ -1,5 +1,5 @@
 "use client";
-import { Analytics } from '@vercel/analytics/react';
+// import { Analytics } from '@vercel/analytics/react';
 import React from "react";
 import { useState } from "react";
 const Bot = () => {
@@ -14,7 +14,7 @@ const Bot = () => {
         onClick={handleClick}
         className=" bottom-20 right-0 fixed m-8 bg-violet-50 border-violet-600 shadow-xl w-fit bg-opacity-80 backdrop-blur-sm p-4 border-2 cursor-pointer rounded-full hover:scale-110 transition-all text-white"
       >
-        <Analytics />
+        {/* <Analytics /> */}
         <img src="/bot.png" height={40} width={40} alt="" />
       </div>
       <div
