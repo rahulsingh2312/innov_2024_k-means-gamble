6daac673592d2939d50ede9f819110cd7b588651
Yash <yashmadhani2810@gmail.com>
2024-02-27 14:09:40 +0530

    Landing page 2.0

--- markinsights-frontend/src/components/ui/hero-parallax.js
+++ markinsights-frontend/src/components/ui/hero-parallax.js
@@ -97,9 +97,9 @@ export const HeroParallax = ({
 
 export const Header = () => {
   return (
-    <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
+    <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        The Ultimate <br /> development studio
+        <p className='text-red-950 p-0 m-0 inline-block'>MarkInSights</p> <br /> Customer Segmentation and Targeting
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
         We build beautiful products with the latest technologies and frameworks.
