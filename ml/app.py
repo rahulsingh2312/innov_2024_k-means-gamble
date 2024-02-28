57c4581f15195f88893ac57ea2a1afb63c189e55
siro844 <140879194+siro844@users.noreply.github.com>
2024-02-28 07:43:48 +0530

    misc

--- ml/app.py
+++ ml/app.py
@@ -64,10 +64,10 @@ def upload_file():
         # Load the user-uploaded data into a DataFrame
         user_data = pd.read_csv(uploaded_file)
         user_data = user_data.drop(columns='CustomerID')
-        email_data = user_data['Email'].to_json(orient='records')
-        # email_data = email_data.to_array()
-        print(email_data)
-        user_data = user_data.drop(columns='Email')
+        # email_data = user_data['Email'].to_json(orient='records')
+        # # email_data = email_data.to_array()
+        # print(email_data)
+        # user_data = user_data.drop(columns='Email')
         # print(user_data)
         # Check if the required column is present in the user-uploaded data
         if 'Gender' in user_data.columns:
@@ -93,7 +93,7 @@ def upload_file():
             indices_of_three = [i for i, x in enumerate(my_arr) if x == 3]
             indices_of_four = [i for i, x in enumerate(my_arr) if x == 4]
             indices_of_five = [i for i, x in enumerate(my_arr) if x == 5]
-            output = {"predictions": predictions.tolist(), "count_0": counts[0], "count_1": counts[1], "count_2": counts[2], "count_3": counts[3], "count_4": counts[4], "count_5": counts[5], "indices_of_0": indices_of_zero,"indices_of_1": indices_of_one, "indices_of_2": indices_of_two, "indices_of_3": indices_of_three, "indices_of_4": indices_of_four, "indices_of_5": indices_of_five, "email": email_data}
+            output = {"predictions": predictions.tolist(), "count_0": counts[0], "count_1": counts[1], "count_2": counts[2], "count_3": counts[3], "count_4": counts[4], "count_5": counts[5], "indices_of_0": indices_of_zero,"indices_of_1": indices_of_one, "indices_of_2": indices_of_two, "indices_of_3": indices_of_three, "indices_of_4": indices_of_four, "indices_of_5": indices_of_five}
             return jsonify(output)
 
         else:
